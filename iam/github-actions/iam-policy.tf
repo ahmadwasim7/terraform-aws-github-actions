0d9d404b4e249392adc15cd3a84a5dfe0635e224
Wasim Ahmad <wasim@local.com>
2025-12-25 19:05:03 +0530

    corrected iam-policy and iam-role

--- iam/github-actions/iam-policy.tf
+++ iam/github-actions/iam-policy.tf
@@ -1,6 +1,6 @@
-resource "aws_iam_policy" "ec2_full_access_custom" {
-  name        = "EC2FullAccess-2"
-  description = "Full EC2 access only"
+resource "aws_iam_policy" "terraform_ci_infra_policy" {
+  name        = "TerraformCI-InfrastructurePolicy"
+  description = "Permissions for Terraform CI/CD to manage AWS infrastructure and remote state"
 
   policy = jsonencode({
     Version = "2012-10-17"
--- iam/github-actions/iam-role.tf
+++ iam/github-actions/iam-role.tf
@@ -12,8 +12,14 @@ resource "aws_iam_role" "github_actions_oidc" {
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/${var.github_branch}"
+            "token.actions.githubusercontent.com:aud"        = "sts.amazonaws.com"
+            "token.actions.githubusercontent.com:repository" = "${var.github_org}/${var.github_repo}"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/${var.github_branch}",
+              "repo:${var.github_org}/${var.github_repo}:pull_request*"
+            ]
           }
         }
       }
@@ -23,5 +29,5 @@ resource "aws_iam_role" "github_actions_oidc" {
 
 resource "aws_iam_role_policy_attachment" "attach_ec2_policy" {
   role       = aws_iam_role.github_actions_oidc.name
-  policy_arn = aws_iam_policy.ec2_full_access_custom.arn
+  policy_arn = aws_iam_policy.terraform_ci_infra_policy.arn
 }
